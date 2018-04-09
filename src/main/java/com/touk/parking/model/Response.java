f89bbe31d0735acd3e2f31cf48c2800d08b496c9
asole83 <asolevall@gmail.com>
2018-04-09 17:45:19 +0200

    Adding file of JSON Response

---
+++ src/main/java/com/touk/parking/model/Response.java
@@ -0,0 +1,13 @@
+package com.touk.parking.model;
+
+public class Response {
+    private final String content;
+
+    public Response(String content) {
+        this.content = content;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+}
